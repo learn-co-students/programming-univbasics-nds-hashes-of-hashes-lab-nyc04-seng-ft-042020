b05e33e18f42cfe3daa42b037d874ef25fe3a77f
Kyle B <kjburda@gmail.com>
2020-02-20 16:32:03 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,42 +4,47 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+ PORTION_1 = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
+ 
+ PORTION_2 = {
+   label: "Order"
+ }
+ 
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+ 
+  PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+}
+
 
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+  
+  
+  PORTION_1[:sub_category][:sub_category][:sub_category] = (PORTION_2)
+  PORTION_1
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  PORTION_1
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
+  
+  domain = PORTION_1
+  
 end
+
+# Is there a more effective method/approach to build nested hash of hash? I
+# avoided using literals, but the above feels very ineffective when dealing
+# with larger nodes. I wonder what kind of algorithm would be used in
+# businesses reliant on big data.
